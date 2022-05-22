8ffd338b4c6f3f965c9c2dae87749c26bac395d7
siegfried bozza <siegfried.bozza@protonmail.com>
2022-05-22 18:07:40 +0200

    updated restaurant controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding database with restaurants..."
+25.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+
+puts "Database seeded with restaurants."
