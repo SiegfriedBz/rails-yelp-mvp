339956f9ae372b3b0e9859ad6687093708e48dfb
siegfried bozza <siegfried.bozza@protonmail.com>
2022-05-22 18:44:52 +0200

    updated reviews routes, controller, views

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian)}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
 end
